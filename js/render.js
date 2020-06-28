b63c7878b12234cae8e12a771817bdee611e2b84
unknown <conlocxoaythuytrieu@gmail.com>
2020-06-28 17:30:39 +0700

    pointtttttt

--- js/render.js
+++ js/render.js
@@ -7,11 +7,11 @@ var init = function () {
 	scene.background = new THREE.Color(0x343a40);
 
 	// grid
-	var gridHelper = new THREE.GridHelper(1000, 100, 0xffffff, 0xffffff);
+	var gridHelper = new THREE.GridHelper(400, 50, 0xffffff, 0xffffff);
 	scene.add(gridHelper);
 
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.set(1, 15, 20);
+	camera.position.set(1, 50, 100);
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 	renderer = new THREE.WebGLRenderer();
@@ -34,24 +34,13 @@ init();
 var geo = new THREE.Mesh();
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 // CUBE
-var BoxGeometry = new THREE.BoxGeometry(5, 5, 5);
-var SphereGeometry = new THREE.SphereGeometry(3, 50, 50);
-var ConeGeometry = new THREE.CylinderGeometry(0, 2, 6, 50, 1);
-var CylinderGeometry = new THREE.CylinderGeometry(2, 2, 4, 30);
-var TorusGeometry = new THREE.TorusGeometry(2, 0.5, 20, 100);
-var TeapotGeometry = new THREE.TeapotBufferGeometry(2, 2, 4);
-/*
-- Hình hộp: BoxGeometry(a, b, c)
-- Hình Cầu: SphereGeometry( 3, 50, 50 )
-- Hình nón: CylinderGeometry(0, 2, 6, 50, 1)
-- Hình Trụ: CylinderGeometry( 2, 2, 4, 30 );
-- Bánh xe: TorusGeometry( 2, 0.5, 20, 100 )
-- Ấm trà: TeapotBufferGeometry( 2, 2, 4);
+var BoxGeometry = new THREE.BoxGeometry(50, 50, 50, 40, 40, 40);
+var SphereGeometry = new THREE.SphereGeometry(30, 60, 60);
+var ConeGeometry = new THREE.CylinderGeometry(0, 20, 60, 50, 10);
+var CylinderGeometry = new THREE.CylinderGeometry(20, 20, 40, 300);
+var TorusGeometry = new THREE.TorusGeometry(20, 5, 20, 100);
+var TeapotGeometry = new THREE.TeapotBufferGeometry(20, 8);
 
-*/
-// Create a MeshFaceMaterial, which allows the cube to have different materials on each face
-// var cubeMaterial = new THREE.MeshFaceMaterial( cubeMaterials );
-// var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
 var Material;
 
 // var directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
@@ -60,57 +49,80 @@ var Material;
 
 // scene.fog = new THREE.Fog(0x999999, 0, 20000);
 // renderer.setClearColor(scene.fog.color, 1);
-
-var SetMeterial = function (type){
+var type = 3, d_id;
+var SetMeterial = function (x){
+	type = x;
 	switch(type)
 	{
 		case 1:
+			Material = new THREE.PointsMaterial({
+				color: 0xffffff,
+				size: 0.2,
+			});
 			break;
 		case 2:
 			Material = new THREE.MeshBasicMaterial({
-				color: 0xff0000,
+				color: 0xffffff,
 				wireframe: true,
 			});
 			break;
 		case 3:
 			Material = new THREE.MeshBasicMaterial({
-				color: 0xff0000,
+				color: 0xffffff,
 			});
 			break;
 	}
+	AddGeo(d_id);
 }
 
 var AddGeo = function (id) {
-	SetMeterial(2);
-	// controls.update();
+	// console.log(type);
+	controls.update();
 	if (id>0 && id<7)
 		{
+			d_id = id;
 			scene.remove(geo);
-			geo.remove();
 		}
 	switch (id) {
 		case 1:
+			if (type == 1)
+				geo = new THREE.Points(BoxGeometry, Material);
+			else
 			geo = new THREE.Mesh(BoxGeometry, Material);
 			break;
 		case 2:
+			if (type == 1)
+				geo = new THREE.Points(SphereGeometry, Material);
+			else
 			geo = new THREE.Mesh(SphereGeometry, Material);
 			break;
 		case 3:
+			if (type == 1)
+				geo = new THREE.Points(ConeGeometry, Material);
+			else
 			geo = new THREE.Mesh(ConeGeometry, Material);
 			break;
 		case 4:
+			if (type == 1)
+				geo = new THREE.Points(CylinderGeometry, Material);
+			else
 			geo = new THREE.Mesh(CylinderGeometry, Material);
 			break;
 		case 5:
+			if (type == 1)
+				geo = new THREE.Points(TorusGeometry, Material);
+			else
 			geo = new THREE.Mesh(TorusGeometry, Material);
 			break;
 		case 6:
+			if (type == 1)
+				geo = new THREE.Points(TeapotGeometry, Material);
+			else
 			geo = new THREE.Mesh(TeapotGeometry, Material);
 			break;
 	}
 
-	geo.rotation.x += 0.01; // animation
-	console.log(geo.rotation.x);
+	// geo.rotation.x += 0.01; // animation
 	scene.add(geo);
 	renderer.render(scene, camera);
 	requestAnimationFrame(AddGeo);
--- js/test_demo.js
+++ js/test_demo.js
@@ -1,101 +1,84 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var container;
+  var scene, camera, renderer, controls;
+  var geometry, material, mesh;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-console.log(window.innerWidth, window.innerHeight);
-document.getElementById("rendering").appendChild(renderer.domElement);
+  init();
+  animate();
 
-// check when the browser size has changed and adjust the camera accordingly
-window.addEventListener('resize', function () {
-    var WIDTH = window.innerWidth;
-    var HEIGHT = window.innerHeight;
+  function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 5, 5000);
+    camera.position.z = 2750;
 
-    camera.aspect = WIDTH / HEIGHT;
-    camera.updateProjectionMatrix();
+    //Add a buffer geometry for particle system
+    var geometry = new THREE.BufferGeometry();
+    var particles = topology['geometry'];
+    var geometry = new THREE.BufferGeometry();
+    var positions = new Float32Array(particles * 3);
+    var colors = new Float32Array(particles * 3);
+    var color = new THREE.Color();
 
-    renderer.setSize(WIDTH, HEIGHT);
-});
-
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-// CUBE
-var BoxGeometry = new THREE.BoxGeometry(2, 2, 2);
-var SphereGeometry = new THREE.SphereGeometry( 3, 50, 50 );
-var ConeGeometry = new THREE.CylinderGeometry(0, 2, 6, 50, 1);
-var CylinderGeometry = new THREE.CylinderGeometry( 2, 2, 4, 30 );
-var TorusGeometry = new THREE.TorusGeometry( 2, 0.5, 20, 100 );
-var TeapotGeometry = new THREE.TeapotBufferGeometry( 2, 2, 4);
-/*
-- Hình hộp: BoxGeometry(a, b, c)
-- Hình Cầu: SphereGeometry( 3, 50, 50 )
-- Hình nón: CylinderGeometry(0, 2, 6, 50, 1)
-- Hình Trụ: CylinderGeometry( 2, 2, 4, 30 );
-- Bánh xe: TorusGeometry( 2, 0.5, 20, 100 )
-- Ấm trà: TeapotBufferGeometry( 2, 2, 4);
-
-*/
-// Create a MeshFaceMaterial, which allows the cube to have different materials on each face
-// var cubeMaterial = new THREE.MeshFaceMaterial( cubeMaterials );
-// var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
-var Material = new THREE.MeshBasicMaterial( { color: 0xffffff} );
-var PointMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-var LineMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-var SolidMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-
-var directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
-directionalLight.position.set(0, 5, 0);
-scene.add(directionalLight)
-
-scene.fog = new THREE.Fog(0x000000, 0, 20000);
-renderer.setClearColor(scene.fog.color, 1);
-camera.position.z = 10;
-
-var AddGeo = function (id) {
-    // controls.update();
-    
-    while(scene.children.length > 0){ 
-        scene.remove(scene.children[0]); 
-    }
-    switch(id)
+    var i = 0;
+    for (point in topology['geometry'])
     {
-        case 1: 
-            // geo = new THREE.Mesh(BoxGeometry, Material);
-            geo = new THREE.Mesh(BoxGeometry, Material);
-            break;
-        case 2: geo = new THREE.Mesh(SphereGeometry, Material);break;
-        case 3: geo = new THREE.Mesh(ConeGeometry, Material);break;
-        case 4: geo = new THREE.Mesh(CylinderGeometry, Material);break;
-        case 5: geo = new THREE.Mesh(TorusGeometry, Material);break;
-        case 6: geo = new THREE.Mesh(TeapotGeometry, Material);break;
+      var x = point[0];
+      var y = point[1];
+      var z = point[2];
+    
+      //Store the position of the point
+      positions[i]     = x;
+      positions[i + 1] = y;
+      positions[i + 2] = z;
+
+      //Assign a colour to the point
+      color.setRGB(0.42, 0.42, 0.42);
+      colors[i]     = color.r;
+      colors[i + 1] = color.g;
+      colors[i + 2] = color.b;
+      i+=1;
     }
-    
-    geo.rotation.x += 0.01; // animation
-    scene.add(geo);
+
+    geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+    geometry.computeBoundingSphere();
+
+    var material = new THREE.PointCloudMaterial({ size: 15, vertexColors: THREE.VertexColors });
+    particleSystem = new THREE.PointCloud(geometry, material);
+    scene.add(particleSystem);
+
+    //Lights
+    light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    //Set up renderer
+    renderer = new THREE.WebGLRenderer({ antialias:false });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    //Attach renderer to #container DOM element
+    container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+
+    //Add window listener for resize events
+    window.addEventListener('resize', onWindowResize, false);
+
+    //Call render loop
+    animate();
+  }
+
+  function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    console.log(id)
-    requestAnimationFrame(AddGeo);
-}
-// run game loop (update, render, repeat)
-var GameLoop = function () {
-    // controls.update();
-    // var Material = new THREE.PointsMaterial({
-    //     color: 0xffffff,
-    //     size: 0.1,
-    //  });
-    // geo = new THREE.Points(BoxGeometry, Material);
+  }
 
-    
-    var geometry = new THREE.TeapotBufferGeometry( 2, 2, 4);
-
-    var wireframe = new THREE.WireframeGeometry( geometry );
-
-    var line = new THREE.LineSegments( wireframe );
-    line.material.depthTest = false;
-    line.material.opacity = 0.25;
-    line.material.transparent = true;
-
-    scene.add( line );
-    // requestAnimationFrame(GameLoop);
-};
-
-GameLoop();
+  function render(){
+    renderer.render(scene, camera);
+  }
