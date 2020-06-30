4e0b239907161e4d33f8ca0f09f5a0534c33a379
Quan <quantran.quou.6666@gmail.com>
2020-06-30 14:42:13 +0700

    :)

--- js/test_demo.js
+++ js/test_demo.js
@@ -2,24 +2,30 @@ import * as THREE from '../js/three.module.js';
 
 import {
 	OrbitControls
-} from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+} from '../js/OrbitControls.js';
 import {
 	TransformControls
-} from '../jsm/TransformControls.js';
+} from '../js/TransformControls.js';
 import {
 	TeapotBufferGeometry
-} from "https://threejs.org/examples/jsm/geometries/TeapotBufferGeometry.js";
+} from '../js/TeapotBufferGeometry.js';
+import {
+	Projector
+} from '../js/Projector.js';
 
 var cameraPersp, cameraOrtho, currentCamera;
 var scene, renderer, control, orbit;
-var geo = new THREE.Mesh();
-var Material;
-var BoxGeometry = new THREE.BoxGeometry(50, 50, 50, 40, 40, 40);
+var geo;
+var Material = new THREE.MeshBasicMaterial({
+	color: '#F5F5F5',
+});;
+var BoxGeometry = new THREE.BoxGeometry(50, 50, 50, 20, 20, 20);
 var SphereGeometry = new THREE.SphereGeometry(30, 60, 60);
 var ConeGeometry = new THREE.ConeGeometry(20, 60, 50, 20);
-var CylinderGeometry = new THREE.CylinderGeometry(20, 20, 40, 50, 30);
+var CylinderGeometry = new THREE.CylinderGeometry(20, 20, 40, 50, 20);
 var TorusGeometry = new THREE.TorusGeometry(20, 5, 20, 100);
 var TeapotGeometry = new TeapotBufferGeometry(20, 8);
+var texture;
 
 init();
 render();
@@ -27,21 +33,23 @@ render();
 function init() {
 	// Scene
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0x343a40);
+	scene.background = new THREE.Color('#343A40');
 
 	// Grid
-	scene.add(new THREE.GridHelper(400, 50, 0xA3BAC3, 0xA3BAC3));
+	scene.add(new THREE.GridHelper(400, 50, '#A3BAC3', '#A3BAC3'));
 
 	// Coordinate axes
 	scene.add(new THREE.AxesHelper(100));
 
 	// Camera
-	const aspect = window.innerWidth / window.innerHeight;
-	cameraPersp = new THREE.PerspectiveCamera(75, aspect, 0.01, 2000);
-	// cameraOrtho = new THREE.OrthographicCamera(-600 * aspect, 600 * aspect, 600, -600, 0.01, 30000);
-	currentCamera = cameraPersp;
-	currentCamera.position.set(1, 50, 100);
-	currentCamera.lookAt(0, 0, 0);
+	{
+		const aspect = window.innerWidth / window.innerHeight;
+		cameraPersp = new THREE.PerspectiveCamera(75, aspect, 0.01, 2000);
+		// cameraOrtho = new THREE.OrthographicCamera(-600 * aspect, 600 * aspect, 600, -600, 0.01, 30000);
+		currentCamera = cameraPersp;
+		currentCamera.position.set(1, 50, 100);
+		currentCamera.lookAt(0, 0, 0);
+	}
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -50,23 +58,19 @@ function init() {
 
 	// check when the browser size has changed and adjust the camera accordingly
 	window.addEventListener('resize', function () {
-		var WIDTH = window.innerWidth;
-		var HEIGHT = window.innerHeight;
+		const WIDTH = window.innerWidth;
+		const HEIGHT = window.innerHeight;
 
-		camera.aspect = WIDTH / HEIGHT;
-		camera.updateProjectionMatrix();
+		currentCamera.aspect = WIDTH / HEIGHT;
+		currentCamera.updateProjectionMatrix();
 
 		renderer.setSize(WIDTH, HEIGHT);
+		render();
 	});
 
-	// var light = new THREE.DirectionalLight(0xffffff, 2);
+	// var light = new THREE.DirectionalLight('#FFFFFF', 2);
 	// light.position.set(1, 1, 1);
 	// scene.add(light);
-
-	// var texture = new THREE.TextureLoader().load('/img/1.png', render);
-	// texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
-
-	// var material = new THREE.MeshLambertMaterial({ map: texture, transparent: true });
 
 	orbit = new OrbitControls(currentCamera, renderer.domElement);
 	orbit.update();
@@ -79,63 +83,74 @@ function init() {
 		orbit.enabled = !event.value;
 	});
 
+	SetPointLight();
 }
-
 var type = 3,
-	d_id;
+	d_id = null,
+	light = 0;
 
-function SetMaterial(x) {
-	type = x;
+function setTexture(url) {
+	if (d_id == null) return;
+	texture = new THREE.TextureLoader().load(url, render);
+	texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+	SetMaterial(4);
+}
+window.setTexture = setTexture;
+
+
+function SetMaterial(material_id) {
+	if (d_id == null) return;
+	type = material_id;
 	switch (type) {
 		case 1:
 			Material = new THREE.PointsMaterial({
-				color: 0xffffff,
-				size: 0.2,
+				color: '#FFFFFF',
+				sizeAttenuation: false,
+				size: 1,
 			});
 			break;
 		case 2:
 			Material = new THREE.MeshBasicMaterial({
-				color: 0xffffff,
+				color: '#FFFFFF',
 				wireframe: true,
 			});
 			break;
 		case 3:
-			Material = new THREE.MeshBasicMaterial({
-				color: 0xffffff,
-			});
+			if (light == 0)
+				Material = new THREE.MeshBasicMaterial({
+					color: '#FFFFFF',
+				});
+			else
+				Material = new THREE.MeshPhongMaterial({
+					color: '#FFFFFF',
+				});
+			break;
+		case 4:
+			if (light == 0)
+				Material = new THREE.MeshBasicMaterial({
+					map: texture,
+					transparent: true
+				});
+			else
+				Material = new THREE.MeshLambertMaterial({
+					map: texture,
+					transparent: true
+				});
 			break;
 	}
-	AddGeo(d_id);
+	AddGeo(d_id, geo.position);
 }
 window.SetMaterial = SetMaterial;
 
-function setFOV(value) {
-	currentCamera.fov = Number(value);
-	currentCamera.updateProjectionMatrix();
-	render();
-}
-window.setFOV = setFOV;
-
-function setFar(value) {
-	currentCamera.far = Number(value);
-	currentCamera.updateProjectionMatrix();
-	render();
-}
-window.setFar = setFar;
-
-function setNear(value) {
-	currentCamera.near = Number(value);
-	currentCamera.updateProjectionMatrix();
-	render();
-}
-window.setNear = setNear;
-
-function AddGeo(id) {
-	if (id > 0 && id < 7) {
-		d_id = id;
+function AddGeo(geo_id, position = null) {
+	if (geo_id > 0 && geo_id < 7) {
+		d_id = geo_id;
 		scene.remove(geo);
 	}
-	switch (id) {
+
+	mesh_id = 1;
+
+	switch (geo_id) {
 		case 1:
 			if (type == 1)
 				geo = new THREE.Points(BoxGeometry, Material);
@@ -173,30 +188,85 @@ function AddGeo(id) {
 				geo = new THREE.Mesh(TeapotGeometry, Material);
 			break;
 	}
-	// geo.rotation.x += 0.01; // animation
+	geo.id = id;
 
+	console.log(geo);
+	const object = scene.getObjectByProperty('uuid', i);
+	console.log(object);
+	if (position != null) {
+		geo.position.set(position['x'], position['y'], position['z']);
+	}
 	scene.add(geo);
+	control_transform(geo);
+	render();
+}
+window.AddGeo = AddGeo;
 
+function control_transform(geo) {
 	control.attach(geo);
 	scene.add(control);
 	window.addEventListener('keydown', function (event) {
 		switch (event.keyCode) {
 			case 87: // W
-				control.setMode("translate");
+				EventTranslate();
 				break;
 			case 69: // E
-				control.setMode("rotate");
+				EventRotate();
 				break;
 			case 82: // R
-				control.setMode("scale");
+				EventScale();
 				break;
 		}
 	});
+}
 
+function setFOV(value) {
+	currentCamera.fov = Number(value);
+	currentCamera.updateProjectionMatrix();
 	render();
 }
-window.AddGeo = AddGeo;
+window.setFOV = setFOV;
+
+function setFar(value) {
+	currentCamera.far = Number(value);
+	currentCamera.updateProjectionMatrix();
+	render();
+}
+window.setFar = setFar;
+
+function setNear(value) {
+	currentCamera.near = Number(value);
+	currentCamera.updateProjectionMatrix();
+	render();
+}
+window.setNear = setNear;
+
+function EventTranslate() {
+	control.setMode("translate");
+}
+window.EventTranslate = EventTranslate;
+
+function EventRotate() {
+	control.setMode("rotate");
+}
+window.EventRotate = EventRotate;
+
+function EventScale() {
+	control.setMode("scale");
+}
+window.EventScale = EventScale;
+
+function SetPointLight() {
+	let color = '#FFFFFF';
+	let intensity = 1;
+	let light = new THREE.PointLight(color, intensity);
+	light.position.set(0, 50, 0);
+	scene.add(light);
+	const helper = new THREE.PointLightHelper(light);
+	scene.add(helper);
+}
 
 function render() {
 	renderer.render(scene, currentCamera);
+	// console.log(scene.children)
 }
